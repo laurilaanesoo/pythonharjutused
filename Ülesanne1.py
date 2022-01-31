b8b4f40b1edf5f47af572d4b1414227d01e648a4
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-01-31 13:34:15 +0200

    Ülesanne1

---
+++ Ülesanne1.py
@@ -0,0 +1,16 @@
+# Harjutus 1
+# Lauri Laanesoo
+# 31.01.2022
+
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+
+print("   @..@")
+print("  (----)")
+print(" ( \__/ )")
+print("^^  \"\" ^^")
+
+a= int(input("Sisesta ruudu külje pikkus: "))
+s= a*a
+print("Vastus: ruudu pindala on ", str(s)+"cm^2")
