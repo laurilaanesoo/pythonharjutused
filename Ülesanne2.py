475e135258ffc517db9f06cc7a42e4d78755e0c8
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-01-31 13:59:08 +0200

    Ülesanne2

---
+++ Ülesanne2.py
@@ -0,0 +1,43 @@
+# Harjutus 2
+# Lauri Laanesoo
+# 31.01.2022
+
+#Ajateisendus
+minutid = input("Sisesta aeg minutites: ")
+tunnid = minutid/60
+print(minutid "minutit on" ,tunnid, "h")
+
+
+#Kolmnurga hüpotenuus
+a = 16
+b = 9
+hupotenuus = 16^2 + 9^2
+print("Kolmnurga hüpotenuus on",hupotenuus, "cm")
+
+
+
+#Rulluisutajad
+kiirus = 29.9
+aeg = 24
+pikkus = 29.9/24
+print("Rulluisutaja jõuab",pikkus, "km")
+
+#Pitsa
+pitsa = 12.9
+sobrad = 3
+jootraha = 0.1
+maksma = 12.90*3*0.1
+print("Igaüks peab maksma",maksma, "€")
+
+#Toote hind
+hind = 36.75
+soodus = 0.4
+kogus = 3
+summa = 36.75*0.4*3
+print("Kolme toote summa on:",summa,"€")
+
+
+#Kolmnurga ümbermõõt
+a,b,c = 3,4,5
+p=a+b+c
+print("Kolmnurga ümbermõõt on:",p,"cm")
