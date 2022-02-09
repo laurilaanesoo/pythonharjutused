78335a533362c56a96f2d5ca281bbd6ad9a4515d
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-02-09 15:40:48 +0200

    Ülesanne4

--- Ülesanne4.py
+++ Ülesanne4.py
@@ -2,18 +2,97 @@
 # Lauri Laanesoo
 # 03.02.2022
 
-#Tärnid
-#for i in range(1,6):
-#    for j in range(1,6):
-#        print("* ",end="")
-#    print()
+#Ruutude ja kuupide tabel
 
-for l in range(0,5):
-    a=a+1
-    for k in range (0,a):
-       
+for h in range(1,10**2):
+    print(f"{h:4}")
+
+
+
+#Pank
+konto = 0
+raha = 10000 #int(input("Sisesta raha summa, mis tahad panka panna: "))
+intress = 0.05
+aastad = 5 # int(input("Vali aastad: "))
+konto += raha # Lisab kontole raha
+print(f" {'Aasta':4} {'Algsumma':10} {'Intress':10} {'Aasta lõpuks':10}")
+for i in range(aastad):
+    kasum = konto*intress
+    print(f"{i:4} {konto:10.2f} {kasum:10.2f} {konto+kasum:10.2f}")
+    konto+=kasum
+
+#Arvamismäng
+import random
+
+loop = 1
+korrad = 1
+suv = random.randint(1,10)
+
+while loop==1:
+    
+    if korrad <= 3:
+        arv = int(input("Arva ära arv 1-10: "))
+    else:
+        veel = input("Kas soovid veel mängida? jah/ei: ")
+        
+        if veel =="jah":
+            korrad = 0
+        else:
+            loop = 0
+    korrad +=1
+    
+    if arv==suv:
+        print("Arvasid arvu ära")
+        loop = 0
+    else:
+        print("Arva uuesti")
+    
+    print(suv, arv, korrad)
+
+print("Mäng läbi")
+
+
+#Viisikud
+for g in range(1,100):
+    if g%5==0:
+        print(g)
+    else:
+        print("")
+
+
+#Korrutustabel
+for k in range(1,11):
+    arv = 5
+    print(f"{arv} x {k} = {k*arv}")
+
+
+#Paaris paaritu
+for i in range(1,101):
+    if i%2==0:
+        print(i,"paaris")
+    else:
+        print(i,"paaritu")
+        
+
+#Loto
+import random
+arv = random.randint(10000,99999)
+print(arv)
+
+
+#Tärnid
+for i in range(1,6):
+    for j in range(1,6):
         print("* ",end="")
     print()
+
+for i in range(1,6):
+    print("* "*i)
+
+k=5
+for i in range(1,6):
+    print("* "*k)
+    k = k - 1
 
 #Jalgpalli meeskond
 sugu = input("Sisesta oma sugu: ")
