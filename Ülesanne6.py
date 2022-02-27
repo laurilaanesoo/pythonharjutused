53142e3454d1ed77bac27ec8bc4ea73e905f95d9
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-02-27 15:48:47 +0200

    Ülesanne6

--- Ülesanne6.py
+++ Ülesanne6.py
@@ -1,21 +1,17 @@
-#Ülesanne6
-
 re, kesk, kokku = 0, 0, 0
 erakonnad = []
 
 with open('s6pru_l6ustaraamatus.txt','r') as sobrad:
     for rida in sobrad:
          enimi, vnimi, er, arv = rida.split(" ")
+         print(f"{rida[0]:11} {rida[1]:11}", file=open("nimed", "a"))
          print(f"{enimi:11}{vnimi:11}{er:4}{arv:5}",end="")
          if er=="RE":
-            re=+1
+            re= re + 1
          elif er=="KE":
-            kesk+=1
-         
-         
-
-print(f"Reformikad: {re}\nKesikuid: {kesk}\n")
-print(f"Erakondi kokku: {len(erakonnad)}")
-         
-         
-         
+            kesk= kesk + 1
+         if er not in erakonnad:
+            erakonnad.append(erakonnad)
+             
+    print(f"Reformikad: {re}\nKesikuid: {kesk}\n")
+    print(f"Erakondi kokku: {len(erakonnad)}")
