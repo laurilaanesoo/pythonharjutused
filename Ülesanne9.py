722b299e5634086f40b40497fe63e5cccd7d1041
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-02-28 21:16:34 +0200

    Ülesanne9

--- Ülesanne9.py
+++ Ülesanne9.py
@@ -2,29 +2,35 @@
 #17.02.2022
 #Lauri Laanesoo
 
-
+from dateutil.relativedelta import relativedelta
 import datetime
+
 kuud = ["jaanuar", "veebruar", "märts"]
 
+#Tänane kuupäev
 aeg = datetime.datetime.today()   
 kuupaev = aeg.strftime("%d.%B.%Y")
 kuupaeva = aeg.strftime("%d.%m.%Y") 
 print("Tänane kuupäev on:", kuupaev)
 
-ajad = aeg.strftime("%d.")+kuud[int(aeg.strftime("%m"))-1]+" "+aeg.strftime("%Y")
+#Eesti keelne kuupäev
+kuupaevad = aeg.strftime('%d.%m.%Y')
+d, m, Y = kuupaeva.split('.')
+kuu = kuud[int(m)-1]
+aeg2 = (d +"."+ kuu +"." + Y)
 
-print(ajad)
+print(aeg2)
 
-
-
-
+#Isikukood
 isikukood = "505080501334"
-aasta = int("20"+isikukood[1]+isikukood[2])
+aasta = int(isikukood[1]+isikukood[2])+2000
 kuu = int(isikukood[3]+isikukood[4])
 paev = int(isikukood[5]+isikukood[6])
 
-vanus = str(paev)+"."+str(kuu)+"."+str(aasta)
-from dateutil.relativedelta import relativedelta
+kokku = datetime.datetime(aasta, kuu, paev)
+ajad = datetime.datetime(int(aeg.strftime(" %Y")), int(aeg.strftime(" %m")), int(aeg.strftime(" %d")))
 
-erinevus = relativedelta(kuupaeva,vanus)
+erinevus = relativedelta(ajad,kokku).years
 print("Aasta vahe on: "+str(erinevus))
+
+
